8235e6bdab0ecdc67a4366f8b4f6ef4ff49ca3fe
woodp1anks <woodteacup@outlook.com>
2022-08-04 14:46:50 +0800

    Better NoRotateSet

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoRotateSet.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/NoRotateSet.kt
@@ -7,19 +7,26 @@ package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.ListValue
 import net.minecraft.network.play.client.C03PacketPlayer.C05PacketPlayerLook
 import net.minecraft.network.play.server.S08PacketPlayerPosLook
 
 @ModuleInfo(name = "NoRotateSet", description = "Prevents the server from rotating your head.", category = ModuleCategory.MISC)
 class NoRotateSet : Module() {
+    private val modeValue = ListValue("Mode", arrayOf("EditPacket","SetAfterPacket"),"SetAfterPacket")
     private val confirmValue = BoolValue("Confirm", true)
     private val illegalRotationValue = BoolValue("ConfirmIllegalRotation", false)
     private val noZeroValue = BoolValue("NoZero", false)
+
+    private var realYaw = 0F
+    private var realPitch = 0F
+    private var needSet = false
 
     @EventTarget
     fun onPacket(event: PacketEvent) {
@@ -39,8 +46,23 @@ class NoRotateSet : Module() {
                     mc.netHandler.addToSendQueue(C05PacketPlayerLook(packet.yaw, packet.pitch, thePlayer.onGround))
             }
 
-            packet.yaw = thePlayer.rotationYaw
-            packet.pitch = thePlayer.rotationPitch
+            if (modeValue.get().equals("EditPacket",true)) {
+                packet.yaw = thePlayer.rotationYaw
+                packet.pitch = thePlayer.rotationPitch
+            } else if (modeValue.get().equals("SetAfterPacket",true)) {
+                needSet = true;
+                realYaw = thePlayer.rotationYaw
+                realPitch = thePlayer.rotationPitch
+            }
+        }
+    }
+
+    @EventTarget
+    fun onUpdate(updateEvent: UpdateEvent) {
+        if (needSet) {
+            needSet = false;
+            mc.thePlayer.rotationYaw = realYaw
+            mc.thePlayer.rotationPitch = realPitch
         }
     }
 
