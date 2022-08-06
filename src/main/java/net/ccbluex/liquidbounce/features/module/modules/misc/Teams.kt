e9d521ca4af123b10b9f75c4631758b7ca3c2ff9
woodp1anks <woodteacup@outlook.com>
2022-08-06 11:24:03 +0800

    Teams armorColor & tower keyBind & ignore exception in Notifications

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/Teams.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/Teams.kt
@@ -10,13 +10,16 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.value.BoolValue
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.init.Items
+import net.minecraft.item.ItemArmor
 
 @ModuleInfo(name = "Teams", description = "Prevents Killaura from attacking team mates.", category = ModuleCategory.MISC)
 class Teams : Module() {
 
-    private val scoreboardValue = BoolValue("ScoreboardTeam", true)
+    private val scoreboardValue = BoolValue("ScoreboardTeam", false)
     private val colorValue = BoolValue("Color", true)
     private val gommeSWValue = BoolValue("GommeSW", false)
+    private val armorColorValue = BoolValue("ArmorColor",true);
 
     /**
      * Check if [entity] is in your own team using scoreboard, name color or team prefix
@@ -43,6 +46,21 @@ class Teams : Module() {
             val clientName = displayName.formattedText.replace("§r", "")
             return targetName.startsWith("§${clientName[1]}")
         }
+
+        try {
+            if (armorColorValue.get() && thePlayer.getCurrentArmor(3).item == Items.leather_helmet && entity.getCurrentArmor(3).item == Items.leather_helmet) {
+                val meHelmet = thePlayer.getCurrentArmor(3).item as ItemArmor
+                val entityHelmet = entity.getCurrentArmor(3).item as ItemArmor
+
+                if (meHelmet.getColor(thePlayer.getCurrentArmor(3)) == entityHelmet.getColor(entity.getCurrentArmor(3))) {
+                    return true
+                }
+            }
+        } catch (_:NullPointerException) {
+
+        }
+
+
 
         return false
     }
